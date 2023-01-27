79314692237eda38409e8416283bc5fd9724794a
clashlover <clashlover927@gmail.com>
2023-01-27 11:14:09 +0530

    added redirect for netlify

--- src/Routes/Users/users.jsx
+++ src/Routes/Users/users.jsx
@@ -16,7 +16,6 @@ const Users = () => {
   useEffect(() => {
     handleUsers();
     console.log(error);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return isPending ? (
     <div>Loading...</div>
